c8d58d6552cb7d688e239083b572578ead682797
Nguyễn Đình Trưng <nguyendinhtrung.it@gmail.com>
2025-10-23 09:59:33 +0700

    up

--- src/utils/disableConsole.js
+++ src/utils/disableConsole.js
@@ -1,10 +1,12 @@
 export const disableConsoleInProduction = () => {
   if (process.env.NODE_ENV === "production") {
-    const noop = () => { };
-    console.log = noop;
-    console.warn = noop;
-    console.error = noop;
-    console.info = noop;
-    console.debug = noop;
+    const originalError = console.error;
+    console.error = (...args) => {
+      // Chặn lỗi từ axios hoặc fetch in ra URL
+      if (typeof args[0] === "string" && args[0].includes("401")) return;
+      if (typeof args[0] === "string" && args[0].includes("api-qtpm-dvn.fly.dev")) return;
+      originalError(...args);
+    };
   }
+
 };
